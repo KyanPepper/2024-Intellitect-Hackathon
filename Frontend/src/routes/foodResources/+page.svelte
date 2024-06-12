0bd24172fb61658ba381a0ffec26e43b77cc3327
Elizabeth Pauley <elizabeth.pauley@intellitect.com>
2024-06-12 08:51:44 -0700

    splitting resources page into categories
    renamed new page to foodResource

--- Frontend/src/routes/foodResources/+page.svelte
+++ Frontend/src/routes/foodResources/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import { onMount } from "svelte";
     import ResourceCard from "$lib/ResourceCard.svelte";
+    
+    let testResource = getasource(1);
   </script>
   
   <div class="w-full h-full">
-    <ResourceCard></ResourceCard>
+    <ResourceCard {}></ResourceCard>
   </div>
